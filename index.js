6e8dab2ccbda996d5032cb0f6e11b0bb0b21d6d1
mieje <mierafjejaw@gmail.com>
2023-06-02 00:11:04 +0300

    create routes

---
+++ index.js
@@ -0,0 +1,21 @@
+import  express  from "express";
+const app = express()
+import authRoutes from "./routes/auth";
+import userRoutes from "./routes/users";
+import postRoutes from "./routes/posts";
+import likeRoutes from "./routes/likes";
+import commentRoutes from "./routes/comments";
+
+//middlewares
+app.use(express.json())
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
+app.use("/api/comments",commentRoutes);
+app.use("/api/likes",likeRoutes);
+
+
+app.listen(8800,()=>{
+ console.log("backend working!")
+})
---
+++ routes/comments.js
@@ -0,0 +1,6 @@
+import express from "expres";
+
+const router = express.Router()
+
+
+export default router
